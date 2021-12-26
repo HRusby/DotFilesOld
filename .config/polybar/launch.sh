12b36379ad5ca84bd303ac9883d35b7ac6cb5afe
HRusby <harrygeorger@hotmail.com>
2021-12-26 14:58:16 +0000

    Removing unused config from polybar

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -4,8 +4,12 @@
 killall -q polybar
 # If all your bars have ipc enabled, you can also use 
 # polybar-msg cmd quit
+barName="mybar"
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+	MONITOR=$m polybar --reload $barName 2>&1 | tee -a /tmp/polybar.log & disown &
+done
 
 # Launch Polybar, using default config location ~/.config/polybar/config
-polybar mybar 2>&1 | tee -a /tmp/polybar.log & disown
+#polybar mybar 2>&1 | tee -a /tmp/polybar.log & disown
 
 echo "Polybar launched..."
