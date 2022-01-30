254ebb74082c631961feeb5b23c5fbb6848e053e
HRusby <harrygeorger@hotmail.com>
2022-01-30 16:36:45 +0000

    Extending Config. ColourSchme, Plugins and options

--- .config/nvim/lua/user/options.lua
+++ .config/nvim/lua/user/options.lua
@@ -18,14 +18,22 @@ local options = {
   hlsearch							= false,								-- Don't highlight search results after searching
   errorbells          	= false,
   scrolloff             = 8,										-- Keep 8 lines above/below the cursor when scrolling
+	sidescrolloff					= 8,
   signcolumn            = "yes",
   colorcolumn           = "100",
   cmdheight             = 2,
   timeoutlen            = 200,
 
   -- CompletionOptions
-  completeopt           = {"menuone", "noinsert", "noselect"}
-
+  completeopt           = {"menuone", "noinsert", "noselect"},
+	clipboard							= "unnamed plus",
+	fileencoding					= "utf-8",
+	pumheight							= 10,
+	smartcase 						= true,
+	splitbelow						= true,
+	splitright						= true,
+	cursorline						= true,
+	wrap									= false
 }
 
 for k, v in pairs(options) do
