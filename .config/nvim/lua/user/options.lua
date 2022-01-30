61c48093cf8533385b3756053471ef2f763f89d4
HRusby <harrygeorger@hotmail.com>
2022-01-30 15:39:56 +0000

    Moving to lua config

---
+++ .config/nvim/lua/user/options.lua
@@ -0,0 +1,33 @@
+tabsInSpaces    = 2                     -- I opt for tabs and spaces to be displayed the same
+local options = {
+  number                = true,                 -- Show LineNumber
+  relativenumber        = true,                 -- Show Line Numbers relative to current line
+
+  tabstop               = tabsInSpaces,         -- # Spaces to display a tab as
+  softtabstop           = tabsInSpaces,         -- # Spaces to remove when backspacing
+  shiftwidth            = tabsInSpaces,         -- # Spaces to display normal mode indenting
+  expandtab             = false,                -- Don't expand tabs to spaces
+  smartindent           = true,                 -- Auto Indent for brace pairs
+
+  swapfile            	= false,
+  backup              	= false,
+  undofile              = true,
+  undodir               = "/home/harry/.vim/undodir",
+
+  -- hidden                = true, -- Defaulted therefore not needed
+  hlsearch							= false,								-- Don't highlight search results after searching
+  errorbells          	= false,
+  scrolloff             = 8,										-- Keep 8 lines above/below the cursor when scrolling
+  signcolumn            = "yes",
+  colorcolumn           = "100",
+  cmdheight             = 2,
+  timeoutlen            = 200,
+
+  -- CompletionOptions
+  completeopt           = {"menuone", "noinsert", "noselect"}
+
+}
+
+for k, v in pairs(options) do
+    vim.opt[k] = v
+end
