aaaf54b8072b16377e79177e2560a96de31bf6d2
HRusby <harrygeorger@hotmail.com>
2022-02-05 17:57:26 +0000

    Fixing options.lua

--- .config/nvim/lua/user/options.lua
+++ .config/nvim/lua/user/options.lua
@@ -1,41 +1,40 @@
-tabsInSpaces    = 2                     -- I opt for tabs and spaces to be displayed the same
+tabsInSpaces					= 4										 -- I opt for tabs and spaces to be displayed the same
 local options = {
-  number                = true,                 -- Show LineNumber
-  relativenumber        = true,                 -- Show Line Numbers relative to current line
+	number						= true,								 -- Show LineNumber
+	relativenumber				= true,								 -- Show Line Numbers relative to current line
 
-  tabstop               = tabsInSpaces,         -- # Spaces to display a tab as
-  softtabstop           = tabsInSpaces,         -- # Spaces to remove when backspacing
-  shiftwidth            = tabsInSpaces,         -- # Spaces to display normal mode indenting
-  expandtab             = false,                -- Don't expand tabs to spaces
-  smartindent           = true,                 -- Auto Indent for brace pairs
+	tabstop						= tabsInSpaces,				 -- # Spaces to display a tab as
+	softtabstop					= tabsInSpaces,				 -- # Spaces to remove when backspacing
+	shiftwidth					= tabsInSpaces,				 -- # Spaces to display normal mode indenting
+	expandtab					= false,								-- Don't expand tabs to spaces
+	smartindent					= true,								 -- Auto Indent for brace pairs
 
-  swapfile            	= false,
-  backup              	= false,
-  undofile              = true,
-  undodir               = "/home/harry/.vim/undodir",
+	swapfile					= false,
+	backup						= false,
+	undofile					= true,
+	undodir						= "/home/harry/.vim/undodir",
 
-  -- hidden                = true, -- Defaulted therefore not needed
-  hlsearch							= false,								-- Don't highlight search results after searching
-  errorbells          	= false,
-  scrolloff             = 8,										-- Keep 8 lines above/below the cursor when scrolling
-	sidescrolloff					= 8,
-  signcolumn            = "yes",
-  colorcolumn           = "100",
-  cmdheight             = 2,
-  timeoutlen            = 200,
+	--hidden						= true, -- Defaulted therefore not needed
+	hlsearch					= false,								-- Don't highlight search results after searching
+	errorbells					= false,
+	scrolloff					= 8,										-- Keep 8 lines above/below the cursor when scrolling
+	sidescrolloff				= 8,
+	signcolumn					= "yes",
+	colorcolumn					= "100",
+	cmdheight					= 2,
+	timeoutlen					= 200,
 
-  -- CompletionOptions
-  completeopt           = {"menuone", "noinsert", "noselect"},
-	clipboard							= "unnamed plus",
-	fileencoding					= "utf-8",
-	pumheight							= 10,
-	smartcase 						= true,
-	splitbelow						= true,
-	splitright						= true,
-	cursorline						= true,
-	wrap									= false
+	-- CompletionOptions
+	completeopt					= {"menuone", "noinsert", "noselect"},
+	fileencoding				= "utf-8",
+	pumheight					= 10,
+	smartcase					= true,
+	splitbelow					= true,
+	splitright					= true,
+	cursorline					= true,
+	wrap						= false
 }
 
 for k, v in pairs(options) do
-    vim.opt[k] = v
+		vim.opt[k] = v
 end
