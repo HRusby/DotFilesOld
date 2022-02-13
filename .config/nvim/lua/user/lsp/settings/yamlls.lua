139392c8918892e3e9c0c539b64de487b411f6a8
HRusby <harrygeorger@hotmail.com>
2022-02-13 11:28:33 +0000

    Adding schemas to yamlls.lua

---
+++ .config/nvim/lua/user/lsp/settings/yamlls.lua
@@ -0,0 +1,9 @@
+return {
+  settings = {
+    yaml = {
+      schemas = {
+        ["https://raw.githubusercontent.com/argoproj/argo-workflows/master/api/jsonschema/schema.json"] = "kubernetes/**/*.yaml"
+      },
+    },
+  },
+}
